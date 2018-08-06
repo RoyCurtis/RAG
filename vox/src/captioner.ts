10b1e93b494da943d21eee1176d103ba5ed18d3c
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2018-08-06 19:47:23 +0100

    Setup vox editor as an electron app

---
+++ vox/src/captioner.ts
@@ -0,0 +1,171 @@
+/** Rail Announcements Generator. By Roy Curtis, MIT license, 2018 */
+
+import {VoxEditor} from "./voxEditor";
+
+/** Represents a dictionary of voice keys and their captions */
+export type PhraseCaptions = {[id: string] : string};
+
+/** Generates a bank of IDs and captions from a given phraseset document and data */
+export class Captioner
+{
+    /** Reference to the generated phrase caption bank */
+    public readonly captionBank : PhraseCaptions = {};
+
+    public constructor()
+    {
+        this.populateLetters();
+        this.populateNumbers();
+        this.populateExcuses();
+        this.populatePhrasesets();
+        this.populateNames();
+        this.populateServices();
+        this.populateStations();
+
+        console.log(Object.keys(this.captionBank).length);
+    }
+
+    /** TreeWalker filter to only accept text nodes */
+    private nodeFilter(node: Node): number
+    {
+        // Only accept text nodes with words in them
+        if (node.textContent!.match(/[a-z0-9]/i))
+            return NodeFilter.FILTER_ACCEPT;
+
+        return NodeFilter.FILTER_REJECT;
+    }
+
+    private populateLetters() : void
+    {
+        // TODO: After moving letters out of I18n, fix this
+        let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        for (let i = 0; i < letters.length; i++)
+        {
+            let letter = letters[i];
+
+            this.captionBank[`letter.${letter}`] = letter;
+        }
+    }
+
+    private populateNumbers() : void
+    {
+        // Single digits
+        for (let n = 0; n <= 60; n++)
+            this.captionBank[`number.${n}`] = n.toString();
+
+        // 24 hour double digits
+        for (let n = 1; n <= 9; n++)
+            this.captionBank[`number.0${n}`] = `Oh-${n}`;
+
+        // 00:MM
+        this.captionBank[`number.00`] = 'Oh-oh';
+
+        // 00:00
+        this.captionBank[`number.0000`] = 'Oh-zero hundred';
+
+        // "Hundred"
+        this.captionBank['number.hundred'] = 'Hundred';
+    }
+
+    private populateExcuses() : void
+    {
+        for (let i = 0; i < VoxEditor.database.excuses.length; i++)
+            this.captionBank[`excuse.${i}`] = VoxEditor.database.excuses[i];
+    }
+
+    /** Walks through every XML element and populates the caption bank from phrasesets */
+    private populatePhrasesets() : void
+    {
+        let treeWalker = document.createTreeWalker(
+            VoxEditor.database.phrasesets,
+            NodeFilter.SHOW_TEXT,
+            { acceptNode: this.nodeFilter },
+            false
+        );
+
+        let lastId  = '';
+        let lastIdx = 0;
+
+        while ( treeWalker.nextNode() )
+        {
+            let current = treeWalker.currentNode;
+            let parent  = current.parentElement!;
+            let psIndex = -1;
+            let id      = '';
+            let value   = Strings.clean(current.textContent!);
+
+            // If text is part of a phraseset, get index of the phrase within the set
+            if ( !parent.hasAttribute('id') )
+            {
+                let phraseSet = parent.parentElement!;
+
+                // https://stackoverflow.com/a/9132575/3354920
+                psIndex = Array.prototype.indexOf.call(phraseSet.children, parent);
+                parent  = phraseSet;
+            }
+
+            // Calculate ID by getting relative indicies of phrases and text parts
+            id = 'phrase.' + parent.id;
+
+            // Append phrase index if we're in a phraseset
+            if (psIndex !== -1)
+                id += `.${psIndex}`;
+
+            // Append the text part's index inside the phrase
+            if (lastId !== id)
+            {
+                lastIdx = 0;
+                lastId  = id;
+            }
+
+            id += `.${lastIdx++}`;
+
+            // Append a "preview" of the next sibling to the text
+            if (current.nextSibling)
+            {
+                let next = current.nextSibling as HTMLElement;
+                let tag  = next.nodeName.toUpperCase();
+
+                // Append extra reference data to tag
+                if      (next.id)
+                    tag += ':' + next.id;
+                else if (next.hasAttribute('context'))
+                    tag += ':' + next.getAttribute('context');
+                else if (next.hasAttribute('ref'))
+                    tag += ':' + next.getAttribute('ref');
+
+                value += ` <${tag}>`;
+            }
+
+            this.captionBank[id] = value;
+        }
+    }
+
+    private populateNames() : void
+    {
+        for (let i = 0; i < VoxEditor.database.named.length; i++)
+            this.captionBank[`named.${i}`] = VoxEditor.database.named[i];
+    }
+
+    private populateServices() : void
+    {
+        for (let i = 0; i < VoxEditor.database.services.length; i++)
+            this.captionBank[`services.${i}`] = VoxEditor.database.services[i];
+    }
+
+    private populateStations() : void
+    {
+        // Filter out parenthesized location context
+        let filter   = (v: string) => v.replace(/\(.+\)/i, '').trim();
+        let stations = VoxEditor.database.stations;
+        let keys     = Object.keys(stations);
+
+        keys.forEach(k =>
+            this.captionBank[`stations.middle.${k}`] = filter(stations[k])
+        );
+
+        keys.forEach(k =>
+            this.captionBank[`stations.end.${k}`] = filter(stations[k])
+        );
+    }
+}
