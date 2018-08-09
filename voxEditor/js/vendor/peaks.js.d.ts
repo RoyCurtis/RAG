403dc9e957a4198335c213fce88392455e5fb581
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2018-08-09 03:38:01 +0100

    Add peaks.js vendor library

---
+++ voxEditor/js/vendor/peaks.js.d.ts
@@ -0,0 +1,7 @@
+/** Rail Announcements Generator. By Roy Curtis, MIT license, 2018 */
+
+/** UI component for interacting with audio waveforms */
+declare class peaks
+{
+    static init() : peaks;
+}
